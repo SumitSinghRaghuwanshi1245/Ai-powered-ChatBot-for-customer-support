54015554ce06895ee23b28d271dfbec3ae7a5f31
SumitSinghRaghuwanshi1245 <sumittsinghr369@gmail.com>
2025-03-30 01:20:40 +0530

    ui

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type React from "react"
+import "@/styles/globals.css"
+import { Inter } from "next/font/google"
+import type { Metadata } from "next"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Chatify",
+  description: "One Stop Playform for resolving the customers Queries!",
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
+
+
+
+import './globals.css'
